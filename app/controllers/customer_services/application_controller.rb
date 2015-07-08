e374ee4a91fe9935ff55dfe1a6d55b6a24b49786
Yomi Colledge <baphled@boodah.net>
2015-07-08 16:18:02 +0100

    Improve navigation for all users types

--- app/controllers/customer_services/application_controller.rb
+++ app/controllers/customer_services/application_controller.rb
@@ -1,13 +1,3 @@
 class CustomerServices::ApplicationController < ApplicationController
   before_action :authenticate_customer_service!
-
-  layout 'customer_services'
-
-  before_action :find_customer_service, if: :current_customer_service
-
-  private
-
-  def find_customer_service
-    @customer_service = current_customer_service
-  end
 end
--- app/controllers/event_managers/application_controller.rb
+++ app/controllers/event_managers/application_controller.rb
@@ -1,14 +1,3 @@
 class EventManagers::ApplicationController < ApplicationController
   before_action :authenticate_event_manager!
-
-  layout 'event_managers'
-
-  before_action :find_event_manager, if: :current_event_manager
-
-  private
-
-  def find_event_manager
-    @event_manager = current_event_manager
-  end
 end
-
