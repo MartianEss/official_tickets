741ca6930b3e127b09263dea1facf12a1a58a1ef
Yomi Colledge <baphled@boodah.net>
2015-07-09 04:06:09 +0100

    Go over the behaviour of requiring an event to be reapproved

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :event_manager
 
-  before_save :needs_reapproval, unless: -> { self.new_record? }
+  before_save :set_approval_status, unless: -> { self.new_record? }
 
   def time_to=(time)
     super time.change(sec: 0)
@@ -11,23 +11,31 @@ class Event < ActiveRecord::Base
     super time.change(sec: 0)
   end
 
+  def approved?
+    approved
+  end
+
   def unapproved?
     !approved
   end
 
+  def needs_reapproval?
+    approved? and requires_reapproval
+  end
+
   protected
 
-  def needs_reapproval
-    self.approved = false if approved? and requires_reverificaiton
+  def set_approval_status
+    self.approved = false if needs_reapproval?
     nil
   end
 
-  def requires_reverificaiton
-    found = needed_to_be_reverification.select {|attr| self.changed.include?(attr) }
+  def requires_reapproval
+    found = needed_to_be_reapproval.select {|attr| self.changed.include?(attr) }
     found.present?
   end
 
-  def needed_to_be_reverification
+  def needed_to_be_reapproval
     %w{location date_from date_to time_to time_from}
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#needs_reapproval?' do
+    it 'checks that the event is approved' do
+      expect(subject).to receive(:needs_reapproval?).and_return true
+      subject.needs_reapproval?
+    end
+
+    it 'checks that it requires reapproval' do
+      expect(subject).to receive(:approved?).and_return true
+      subject.needs_reapproval?
+    end
+  end
+
+  context 'approved event changed' do
+    let(:event_params) do
+      {
+        title:  Faker::Lorem.word,
+        description:  Faker::Lorem.words.join(' '),
+        location: Faker::Address.street_address,
+        contact_number: Faker::PhoneNumber.phone_number,
+
+        genre: Faker::Lorem.word,
+        dress_code: Faker::Lorem.word,
+        date_from: Faker::Date.between(Date.today, Date.today.next_year),
+        date_to: Faker::Date.between(Date.today.tomorrow, Date.today.next_year),
+        time_from: Faker::Time.between(Time.now, Time.now.next_year, :all),
+        time_to: Faker::Time.between(Time.now.tomorrow, Time.now.next_year, :all),
+
+        ticket_type: Faker::Lorem.word,
+        tickets_allocated: rand(1000),
+        price: Faker::Commerce.price,
+
+        approved: true
+      }
+    end
+
+    subject { described_class.create!(event_params) }
+
+    it 'should be approved' do
+      expect(subject).to be_approved
+    end
+
+    context 'attributes that require reapproval' do
+      it 'changes location' do
+        subject.update_attribute(:location, 'London')
+        expect(subject).to be_unapproved
+      end
+
+      it 'changes date from' do
+        subject.update_attribute(:date_from, Date.tomorrow)
+        expect(subject).to be_unapproved
+      end
+
+      it 'changes date to' do
+        subject.update_attribute(:date_to, Date.today.next_year)
+        expect(subject).to be_unapproved
+      end
+
+      it 'changes time from' do
+        subject.update_attribute(:time_from, Time.now.next_year)
+        expect(subject).to be_unapproved
+      end
+
+      it 'changes time to' do
+        subject.update_attribute(:time_to, Time.now.next_year)
+        expect(subject).to be_unapproved
+      end
+    end
+  end
 end
