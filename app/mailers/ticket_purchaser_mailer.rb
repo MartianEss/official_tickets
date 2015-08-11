7dcd4f71b1a72e6b6a62e40d4aa07f0f2a1e01d7
Yomi Colledge <baphled@boodah.net>
2015-08-11 23:11:35 +0100

    Fix issue with sending out emails to ticket purchasers

--- app/mailers/ticket_purchaser_mailer.rb
+++ app/mailers/ticket_purchaser_mailer.rb
@@ -1,7 +1,9 @@
 class TicketPurchaserMailer < ApplicationMailer
   def send_tickets(order)
     
+    @order = order
     @ticket_purchaser = order.ticket_purchaser
+    @tickets_allocation = TicketsAllocation.find(order.tickets_allocation)
     @event = Event.find(order.event_id)
 
     mail(to: @ticket_purchaser.email, subject: "#{@event.title} - Official Tickets")
