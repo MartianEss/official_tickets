285cb288d4be805fc5ff7c178f3a2daa5d09b07c
Yomi Colledge <baphled@boodah.net>
2015-08-11 23:17:15 +0100

    Send ticket email straight away

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,7 +19,7 @@ class OrdersController < ApplicationController
     @order = @event.orders.new(order_params)
 
     if payment_message = @order.process(params[:payment_method_nonce], @tickets_allocation, current_ticket_purchaser)
-      TicketPurchaserMailer.send_tickets(@order).deliver_later
+      TicketPurchaserMailer.send_tickets(@order).deliver_now
       redirect_to event_ticket_orders_path(id: @order, event_id: @event.id, ticket_id: @tickets_allocation.id)
     else
       @client_token = Braintree::ClientToken.generate
