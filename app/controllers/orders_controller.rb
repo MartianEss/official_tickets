a3efcd2917be6c51f057f598683426ade8833bdf
Yomi Colledge <baphled@boodah.net>
2015-08-13 00:46:14 +0100

    Generate PDF and attachment them to the order email

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,8 +19,8 @@ class OrdersController < ApplicationController
 
     if payment_message = @order.process(params[:payment_method_nonce], @tickets_allocation, current_ticket_purchaser)
       @order.save
-      TicketPurchaserMailer.send_tickets(@order).deliver_now
-      redirect_to event_ticket_orders_path(id: @order, event_id: @event.id, ticket_id: @tickets_allocation.id)
+      TicketPurchaserMailer.send_tickets(@order).deliver_later
+      redirect_to event_ticket_order_path(id: @order, event_id: @event.id, ticket_id: @tickets_allocation.id)
     else
       @client_token = Braintree::ClientToken.generate
       flash[:alert] = (payment_message == false) ? 'Unable to process order' : payment_message.processor_response_text
--- app/mailers/ticket_purchaser_mailer.rb
+++ app/mailers/ticket_purchaser_mailer.rb
@@ -6,6 +6,12 @@ class TicketPurchaserMailer < ApplicationMailer
     @tickets_allocation = TicketsAllocation.find(order.tickets_allocation)
     @event = Event.find(order.event_id)
 
+    pdf_job = GeneratePdf.new(order.id)
+    pdf_job.perform
+
+    pdf_path = Rails.root.join('tmp', "#{order.id}.pdf")
+    attachments['tickets.pdf'] = File.read(pdf_path)
+
     mail(to: @ticket_purchaser.email, subject: "#{@event.title} - Official Tickets")
   end
 end
